ad63e693b5eee8f4d343d7de6dd65a362134b52a
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-08 20:29:04 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Home from './components/Home'
 import './App.css';
-
+import { useEffect } from 'react';
+import AOS from "aos"
 
 
 
 function App() {
+  useEffect(()=>{
+    AOS.init();
+    AOS.refresh();
+  },[])
  return (
   <div>
     <Home />
