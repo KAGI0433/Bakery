392e50e139db8dde0860c5d981aa642a3b6e975a
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-08 14:19:36 +0200

    added images

---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Menu.css"
+
+
+const Menu = () => {
+    return (
+        <div className='mx-[320px] mt-[25px]'>
+
+          <div className='flex gap-{51px]'>
+
+            <div className='display-first-section'>
+                <img src="" alt="" />
+            </div>
+          </div>
+            
+        </div>
+    );
+};
+
+export default Menu;
