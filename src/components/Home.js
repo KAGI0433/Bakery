ff617df58fc06beed2a8ea5c9b843947f6d1d223
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-09 11:50:16 +0200

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,6 +12,7 @@ const Home = () => {
             <Navbar />
             <Menu />
             <Products />
+            
         </div>
     );
 };
---
+++ src/components/Slideone/Slideone.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Slideone.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import  "Swiper/css";
+import { Autoplay,Navigation  } from 'swiper';
+import carrot from "../Images/carrot.jpg"
+import choco from 
+
+
+
+
+const Slideone = () => {
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default Slideone;
