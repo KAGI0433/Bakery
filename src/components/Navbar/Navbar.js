b048a8c920b33f0a0ca7b268129ea4e09c54d067
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-10 10:13:07 +0200

    commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,21 +6,16 @@ const Navbar = () => {
     return (
         <div className='navbar-container'>
 
-          <div className='navbar-sub-container'>
-           
-
             <div className='navbar'>
                 <ul className='anim-nav'>
                     <li><a href="#!">Home</a></li>
                     <li><a href="#!">About</a></li>
                     <li><a href="#!">Menu</a></li>
-
-
                 </ul>
+                
                  </div>
-
             </div>
-          </div>
+          
         
     );
 };
